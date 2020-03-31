b4986b80dd3b1c1d6e214a897869fe928d3d1fdc
Devprakash <Devprakashtripathi2438@gmail.com>
2020-03-31 17:00:32 +0530

    my code

---
+++ overloading method.py
@@ -0,0 +1,18 @@
+class Student:
+    def __init__(self, m1, m2):
+        self.m1 = m1
+        self.m2 = m2
+
+    def Sum(self, a=None, b=None, c=None):
+        S = 0
+        if a := None and b:= None and c := none:
+            S = a + b
+        else:
+            s = a
+        return S
+
+
+S1 = Student(58, 69)
+print(S1.Sum(5, 9, 6))
+# print(S1.sum(5,9))
+# print(S.sum(5))
